fe661ef99638774bd4221689e11c45eaab1b564d
polin-drom <avvakumova.p@yandex.ru>
2020-12-22 00:17:09 +0300

    n

--- practice4/pr14.jl
+++ practice4/pr14.jl
@@ -1,24 +1,17 @@
 function mark_kross(r)
-    #include("roblib.jl")
-    for side in (HorizonSide(i) for i = 0:3)
+    include("roblib.jl")
+    for side in (Nord, West, Sud, Ost)
         num = putmarkers!(r, side)
-        moves!(r, inverse(side), num)
+        movements!(r, inverse(side), num)
     end
     putmarker!(r)
 end
 
 function putmarkers!(r::Robot, side::HorizonSide)
     num = 0
-    while !isborder(r, side)
-        move_if_possible(r, side)
-        num += 1
+    while move_if_possible(r, side)
         putmarker!(r)
+        num += 1
     end
     return num
-end
-
-function moves!(r::Robot, side::HorizonSide, num_steps::Int)
-    for i in 1:num_steps
-        move_if_possible!(r, side)
-    end
 end
--- practice4/roblib.jl
+++ practice4/roblib.jl
@@ -12,7 +12,7 @@ movements!(r::Robot, side::HorizonSide, num_steps::Int) = for i in 1:num_steps m
 
 -- Перемещает Робота в заданном направлении до упора    
 """
-movements!(r::Robot, side::HorizonSide) = while isborder(r, side)==false move!(r, side) end 
+#movements!(r::Robot, side::HorizonSide) = while isborder(r, side)==false move!(r, side) end 
 
 
 """
@@ -64,28 +64,39 @@ right(side::HorizonSide) = HorizonSide(mod(Int(side) - 1, 4))
 -- Перемещает Робота в заданном направлении на 1 шаг и возвращает true - это, если мешающей перегородки нет, или, если её можно обойти; имеется ввиду изолированная перегородка прямоугольной формы. 
 В противном случае Робот остается на месте и функция возвращает false.
 """
-function move_if_possible(r::Robot, side::HorizonSide)::Bool
-    orthogonal_side = left(side)
-    reverse_side = inverse(orthogonal_side)
-    num_steps = 0
-    while isborder(r, side)
-        if !isborder(r, orthogonal_side)
-            move(r, orthogonal_side)
-            num_steps += 1
+# Перемещает робота в заданном направлении, если это возможно, и возвращает true,
+    # если перемещение состоялось; в противном случае - false.
+    function move_if_possible!(r::Robot, direct_side::HorizonSide)::Bool
+        orthogonal_side = next(direct_side)
+        reverse_side = reverse(orthogonal_side)
+        num_steps=0
+        while isborder(direct_side) == true
+            if isborder(r, orthogonal_side) == false
+                move(r, orthogonal_side)
+                num_steps += 1
+            else
+                break
+            end
+        end
+        #УТВ: Робот или уперся в угол внешней рамки поля, или готов сделать шаг (или несколько) в направлении 
+        # direct_side
+        if isborder(r,direct_side) == false
+            while isborder(r,reverse_side) == true
+                move!(r,direct_side)
+            end
+            result = true
         else
-            break
+            result = false
         end
+        move!(r,reverse_side)
+        return result
     end
-    #УТВ: Робот или уперся в угол внешней рамки поля, или готов сделать шаг (или несколько) в направлении 
-    # side
-    if !isborder(r, side)
-        while isborder(r, reverse_side)
-            move!(r, side)
-        end
-        result = true
-    else
-        result = false
+
+    # Делает заданное число шагов в заданном направлении, при необходимости обходя внутренние перегородки.
+    # При этом величина одного "шага" может быть больше 1 и равна "толщине" встретившейся прямоугольной 
+    # перегородки
+function movements!(r::Robot, side::HorizonSide, num_steps::Int)
+    for i in 1:num_steps
+        move_if_possible!(r, side) # - в данном случае возможность обхода внутренней перегородки гарантирована
     end
-    movements!(r, reverse_side, num_steps)
-    return result
 end
